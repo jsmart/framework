154983bccedc1a64a6731496c400d006a05d141d
Vadim Shestakov <shestakov2103@gmail.com>
2023-12-03 00:21:13 +0500

    Update DispatchObject

--- src/Events/DispatchObject.php
+++ src/Events/DispatchObject.php
@@ -4,10 +4,22 @@ namespace JSmart\Events;
 
 class DispatchObject
 {
+    private array $properties = [];
+
     public function __construct(array $event = [])
     {
         foreach ($event as $name => $value) {
-            $this->$name = $value;
+            $this->properties[$name] = $value;
         }
+    }
+
+    public function __get($name)
+    {
+        return $this->properties[$name] ?? null;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->properties[$name] = $value;
     }
 }
